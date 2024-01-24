b7aa6a33961255ffdc43e369ab8b0bc82f7f83a2
HoangAnhKun <121826508+HoangAnhKun@users.noreply.github.com>
2024-01-24 22:05:42 +0700

    Update auto-relog.js
    bot auto rejoin

--- snippets/auto-relog.js
+++ snippets/auto-relog.js
@@ -1,10 +1,10 @@
 const mineflayer = require('mineflayer');
 
 var config = {
-  host: 'localhost', // minecraft server ip
+  host: 'PureSurvivalMap.aternos.me', // minecraft server ip
   username: 'email@example.com', // minecraft username
   password: '12345678' // minecraft password, comment out if you want to log into online-mode=false servers
-  // port: 25565,                // only set if you need a port that isn't 25565
+  port: 53146,                // only set if you need a port that isn't 25565
   // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
 }
